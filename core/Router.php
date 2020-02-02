bdd6f8258f363f71a5ddb448a469b15dc04367da
fwlone <fwlone80@gmail.com>
2020-02-02 14:18:05 +0300

    -мелкие правки

--- core/Router.php
+++ core/Router.php
@@ -15,13 +15,13 @@ class Router
         $request = isset($_GET['fw']) && $_GET['fw'] !== '' ? str_replace('..', '', $_GET['fw']) : 'main';
 
         if (substr($request, -1) == '/')
-            $request = $request . 'index';
+            $request = $request . 'index.php';
 
         if (!file_exists(H . '/app/modules/' . $request . '.php'))
-            $request = 'main';
+            $request = 'main.php';
 
         if (!is_file(H . '/app/modules/' . $request . '.php'))
-            throw new \Exception('Модуль не найден: ' . $request);
+            throw new \Exception('File not found: ' . $request);
         else
             return $request;
 
